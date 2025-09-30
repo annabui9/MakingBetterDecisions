9d196b50bd99801dfcc80687529caaadd443bd0d
annabui9 <annabui1009@gmail.com>
2025-09-30 11:36:16 -0500

    added homepage activity that had bottom navigation menu

--- app/src/main/java/com/example/makingbetterdecisions/HomePage.java
+++ app/src/main/java/com/example/makingbetterdecisions/HomePage.java
@@ -26,6 +26,7 @@ public class HomePage extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        //testing
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
