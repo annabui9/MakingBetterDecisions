ad02f4b3fe731fdb63b65587ab1a72cb4654947c
sophiawillingham <sophiwilli@gmail.com>
2025-11-10 21:13:43 -0600

    orb animation

--- app/src/main/java/com/example/makingbetterdecisions/HomeFragment.java
+++ app/src/main/java/com/example/makingbetterdecisions/HomeFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,35 +16,19 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString("param1", param1);
-        args.putString("param2", param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Handle arguments if needed
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Start pulse animation on the glowing orb
-        View orb = view.findViewById(R.id.glowOrb);
-        if (orb != null) {
-            Animation pulse = AnimationUtils.loadAnimation(getContext(), R.anim.pulse);
-            orb.startAnimation(pulse);
-        }
+        // Find the orb view
+        ImageView orb = view.findViewById(R.id.animatedOrb);
+
+        // Load and start the pulse animation
+        Animation pulse = AnimationUtils.loadAnimation(getContext(), R.anim.pulse);
+        orb.startAnimation(pulse);
 
         return view;
     }
 }
+
